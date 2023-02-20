90a8560de84574937257be3d84266e60b2690683
Sensation02 <vasia1415@gmail.com>
2023-02-20 22:23:23 +0200

    updated with new theme + add sm templts fr feature

---
+++ html templates/personal blog/script.js
@@ -0,0 +1,10 @@
+const shareBtns = document.querySelectorAll('.share-btn');
+
+shareBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.preventDefault();
+    const title = document.querySelector('title').textContent;
+    const url = window.location.href;
+    window.open(`https://www.facebook.com/sharer.php?u=${url}&t=${title}`, 'Поділитись', 'width=600,height=400');
+  });
+});
